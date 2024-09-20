2f34a04432979b43694a387848a29a7600309049
vodfr <capuccinuroney@gmail.com>
2024-09-20 22:31:29 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,137 @@
+/*/script/*/
+const links = document.querySelectorAll("a.open");
+const msg = document.querySelector(".message-box");
+const videoElement = document.getElementById("my-video");
+const iframeContainer = document.getElementById("iframe-container");
+var player;
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const clickedLink = link.getAttribute("data-id");
+    iframeContainer.innerHTML = ""; //formater le contenu
+    videoElement.style.display = "block";
+    // Créer une requête XMLHttpRequest pour récupérer les données JSON
+    var xhr = new XMLHttpRequest();
+    var baseURL =
+      "https://raw.githubusercontent.com/ma00tv/ma00tv.github.io/main/JB.json";
+    xhr.open("GET", baseURL, true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].chaine.title === clickedLink) {
+            if (data[i].chaine.protocol === "https") {
+              const type = link.getAttribute("data-youtube");
+              // Initialiser Video.js
+              player = videojs("my-video");
+              // Mettre à jour la source du lecteur
+              player.src({
+                src: data[i].chaine.url,
+                type: player.currentType()
+              });
+              // Charger et jouer la vidéo
+              player.load();
+              player.play();
+              player.controls(true);
+              // Passer en plein écran
+              openFullscreen();
+            } else {
+              player.pause();
+              window.open(data[i].chaine.url);
+            }
+            // Gestion des événements de lecture et de pause
+            player.on("play", function () {
+              msg.style.display = "block";
+              msg.innerHTML = link.textContent + " est en <b>LECTURE...</b>";
+              dialogbox();
+            });
+            player.on("pause", function () {
+              msg.style.display = "block";
+              msg.innerHTML = link.textContent + " est en <b>PAUSE</b>...";
+              dialogbox();
+            });
+          }
+        }
+      }
+    };
+    xhr.send();
+  });
+});
+let startX;
+document.addEventListener("touchstart", function (e) {
+  startX = e.touches[0].clientX;
+});
+document.addEventListener("touchmove", function (e) {
+  let touch = e.touches[0];
+  let deltaX = touch.clientX - startX;
+  if (deltaX > 50) {
+    // Swipe de gauche à droite (ouvrir le menu)
+    document.getElementById("sideMenu").classList.add("open");
+  } else if (deltaX < -50) {
+    // Swipe de droite à gauche (fermer le menu)
+    document.getElementById("sideMenu").classList.remove("open");
+  }
+});
+const b = document.querySelector("button");
+const elem = document.querySelector("body");
+b.addEventListener("click", function () {
+  const api = "https://api.vevioz.com/apis/search/";
+  const d = document.querySelector(".elem");
+  const mp3 = d.value;
+  window.open(api + mp3);
+});
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+function dialogbox() {
+  setTimeout(() => {
+    msg.style.display = "none";
+    msg.innerHTML = "";
+  }, 3000);
+}
+document.querySelectorAll(".iframe").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    msg.style.display = "block";
+    msg.innerHTML = link.textContent + " est en <b>LECTURE...</b>";
+    const iframeSrc = this.getAttribute("data-id");
+    iframeContainer.style.display = "block";
+    videoElement.style.display = "none";
+    playWithIframe(iframeSrc);
+    dialogbox();
+  });
+});
+
+
+function playWithIframe(iframeSrc) {
+  player = videojs("my-video");
+  if (!player.paused()) {
+    // Arrêter la lecture et réinitialiser le lecteur Video.js
+    player.pause();
+    player.src("");
+  }
+  // Vérifier si un iframe existe déjà, sinon le créer
+
+  let iframe = document.getElementById("dynamic-iframe");
+  if (!iframe) {
+    iframe = document.createElement("iframe");
+    iframe.id = "dynamic-iframe";
+    iframeContainer.appendChild(iframe);
+  }
+  // Assigner la source et afficher l'iframe
+  iframe.src = iframeSrc;
+  iframe.width = "100%";
+  iframe.height = "100%";
+  iframe.frameBorder = "0";
+  iframe.allowFullscreen = true;
+  iframe.allowTransparency = true;
+  iframe.style.display = "block";
+                      }
