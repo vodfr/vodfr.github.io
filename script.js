5aba3863a16b3b6d7dc60ba054d9e2b5dc1d95c8
vodfr <capuccinuroney@gmail.com>
2024-09-27 08:08:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-/*/SimpleTV Final by ALGERIA /*/
+/*/SimpleTV /*/
+
 const links = document.querySelectorAll("a.open");
+
 const msg = document.querySelector(".message-box");
+
 const videoElement = document.getElementById("my-video");
+
 const iframeContainer = document.getElementById("iframe-container");
+
 let iframeCreated = false;
+
 let player;
+
 links.forEach((link) => {
+
   link.addEventListener("click", (e) => {
+
     
+
     e.preventDefault();
+
     
+
     const clickedLink = link.getAttribute("data-id");
+
     iframeCreated = false;
+
     iframeContainer.innerHTML = ""; //formater le contenu ->
 
     videoElement.style.display = "block";
@@ -19,27 +33,41 @@ links.forEach((link) => {
     var xhr = new XMLHttpRequest();
 
     var baseURL =
+
       "https://raw.githubusercontent.com/ma00tv/ma00tv.github.io/main/JB.json";
 
     xhr.open("GET", baseURL, true);
 
     xhr.onload = function () {
+
       if (xhr.status === 200) {
+
         var data = JSON.parse(xhr.responseText);
 
         for (var i = 0; i < data.length; i++) {
+
           if (data[i].chaine.title === clickedLink) {
+
             if (data[i].chaine.protocol === "https") {
+
               
+
               
+
               player = videojs("my-video");
+
               player.src({
+
                 src: data[i].chaine.url,
 
                 type: player.currentType()
+
               });
+
               player.ready(function (){ //lecture quant le lecteur est pret
+
                 
+
               player.load();
 
               player.play();
@@ -47,55 +75,87 @@ links.forEach((link) => {
               player.controls(true);
 
               openFullscreen();
+
               });
+
               
+
             } else {
+
               player.pause();
 
               window.open(data[i].chaine.url);
+
             }
 
             player.on("play", function () {
+
               msg.style.display = "block";
 
               msg.innerHTML = link.textContent + " est en <b>LECTURE...</b>";
 
               dialogbox();
+
             });
 
             player.on("pause", function () {
+
               msg.style.display = "block";
 
               msg.innerHTML = link.textContent + " est en <b>PAUSE</b>...";
 
               dialogbox();
+
             });
+
           }
+
         }
+
       }
+
     };
 
     xhr.send();
+
   });
+
 });
+
 let startX;
+
 document.addEventListener("touchstart", function (e) {
+
   startX = e.touches[0].clientX;
+
 });
+
 document.addEventListener("touchmove", function (e) {
+
   let touch = e.touches[0];
 
   let deltaX = touch.clientX - startX;
 
   if (deltaX > 50) {
+
     document.getElementById("sideMenu").classList.add("open");
+
   } else if (deltaX < -50) {
+
     document.getElementById("sideMenu").classList.remove("open");
+
   }
+
 });
+
+
+
 const b = document.querySelector("button");
+
 const elem = document.querySelector("body");
+
 b.addEventListener("click", function () {
+
   const api = "https://api.vevioz.com/apis/search/";
 
   const d = document.querySelector(".elem");
@@ -103,21 +163,35 @@ b.addEventListener("click", function () {
   const mp3 = d.value;
 
   window.open(api + mp3);
+
 });
+
 function dialogbox() {
+
   setTimeout(() => {
+
     msg.style.display = "none";
 
     msg.innerHTML = "";
+
   }, 3000);
+
 }
+
 document.querySelectorAll(".iframe").forEach((link) => {
+
   link.addEventListener("click", function (e) {
+
     
+
     e.preventDefault();
+
     
+
     videoElement.style.display = "none";
+
         
+
     msg.style.display = "block";
 
     msg.innerHTML = link.textContent + " est en <b>LECTURE...</b>";
@@ -131,37 +205,63 @@ document.querySelectorAll(".iframe").forEach((link) => {
     
 
     playWithIframe(iframeSrc);
+
   });
+
 });
+
 function playWithIframe(iframeSrc) {
+
   
+
   setTimeout(function () {
+
     
+
 if (player) {
+
   player.controls(false);
+
   player.pause();
+
   
+
 } else {
+
 player = videojs("my-video");
+
   player.controls(false);
+
   player.pause();
+
  
+
 }
+
   msg.style.display = "block";
+
   msg.innerHTML = "Rétablissement de la chaine ⚠️";
+
   dialogbox();
-  }, 4000);
+
+  }, 5000);
+
   let iframe = document.getElementById("dynamic-iframe");
 
   if (!iframe) {
+
     
+
     iframeCreated = true;
+
     
+
     iframe = document.createElement("iframe");
 
     iframe.id = "dynamic-iframe";
 
     iframeContainer.appendChild(iframe);
+
   }
 
   iframe.src = iframeSrc;
@@ -177,47 +277,56 @@ player = videojs("my-video");
   iframe.allowTransparency = true;
 
   iframe.style.display = "block";
-  
-const message = "1.Balayez dici à gauche&#8592;ou à droite&#8594; pour le menu || 2.ou tapez pour plein écran";
 
-const existingP = document.querySelector("p");
 
-if (existingP) {
-    existingP.remove();
-}
-  
-const dynPar = document.createElement("p");
-  
-dynPar.innerHTML = message;
-  
-  iframeContainer.insertAdjacentElement("beforeend", dynPar);
-
+iframe.style.pointerEvents = 'none'; 
 iframe.addEventListener("load", function () {
+
    
+
    openFullscreen();
+
   
+
    });
   
+
 }
 
-
 window.addEventListener("load", () => {
+
   document.getElementById("sideMenu").classList.add("open");
+
 });
+
 iframeContainer.addEventListener("click", () => {
+
  
+
   if (iframeCreated === true) {
+
     if (iframeContainer.requestFullscreen) {
+
       iframeContainer.requestFullscreen();
+
     } else if (iframeContainer.webkitRequestFullscreen) { // Safari
+
       iframeContainer.webkitRequestFullscreen();
+
     } else if (iframeContainer.msRequestFullscreen) { // IE11
+
       iframeContainer.msRequestFullscreen();
+
     
+
   }
+
     iframeCreated = false;
+
   }
+
 });
+
 function openFullscreen () {
 
   if (elem.requestFullscreen) {
@@ -233,5 +342,7 @@ function openFullscreen () {
       elem.msRequestFullscreen();
 
     } 
+
   
+
   }
