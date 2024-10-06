cf2fe17f8334281e7975fd9e1d9d33a81f84e5ad
vodfr <capuccinuroney@gmail.com>
2024-10-06 12:13:47 +0100

    Update lecteur.js

--- lecteur.js
+++ lecteur.js
@@ -1,51 +1,110 @@
-const startButton = document.getElementById('startRecording');
-        const stopButton = document.getElementById('stopRecording');
-        const downloadLinkContainer = document.getElementById('downloadLinkContainer');
-        const recordingTimeDisplay = document.getElementById('recordingTime');
-        let mediaRecorder;
-        let chunks = [];
-        let recordingInterval;
-        let startTime;
-        function updateRecordingTime() {
-            const elapsedTime = Math.floor((Date.now() - startTime) / 1000); 
-            const minutes = String(Math.floor(elapsedTime / 60)).padStart(2, '0');
-            const seconds = String(elapsedTime % 60).padStart(2, '0');
-            recordingTimeDisplay.textContent = `Temps d'enregistrement : ${minutes}:${seconds}`;
-        }
-        startButton.onclick = () => {
-           const audioContext = new AudioContext();
-           const audioElement = new Audio('https://webradio.tda.dz/Bejaia_64K.mp3');
-           audioElement.crossOrigin = "anonymous"; 
-           audioElement.play();
-           const source = audioContext.createMediaElementSource(audioElement);
-           const streamDestination = audioContext.createMediaStreamDestination();
-           source.connect(streamDestination);
-           source.connect(audioContext.destination);           
-            mediaRecorder = new MediaRecorder(streamDestination.stream);
-            mediaRecorder.ondataavailable = function (e) {
-            chunks.push(e.data);
-            }; 
-            mediaRecorder.start();
-            startTime = Date.now();
-            recordingInterval = setInterval(updateRecordingTime, 1000);
-            startButton.disabled = true;
-            stopButton.disabled = false;
-        };
-        stopButton.onclick = () => {
-            mediaRecorder.stop();
-            stopButton.disabled = true;
-            startButton.disabled = false;
-          clearInterval(recordingInterval);
-            recordingTimeDisplay.textContent = 'Temps d\'enregistrement : 00:00';
-            mediaRecorder.onstop = function () {
-                const blob = new Blob(chunks, { type: 'audio/mpeg' });
-                const audioURL = URL.createObjectURL(blob);
-                const downloadLink = document.createElement('a');
-                downloadLink.href = audioURL;
-                downloadLink.download = 'enregistrement.mp3';
-                downloadLink.textContent = 'Télécharger l\'enregistrement';
-                downloadLinkContainer.innerHTML = '';
-                downloadLinkContainer.appendChild(downloadLink);
-                chunks = [];
-            };
-        };
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Radio béjaia</title>
+  <style>
+    body, html {
+      margin: 0;
+      padding: 0;
+      height: 100%;
+      width: 100%;
+      color: yellow;
+      overflow: hidden;
+      display: flex;
+    }
+
+    body {
+      background: #000;
+    }
+
+    .container {
+      position: relative;
+      width: 100%;
+      height: 100%;
+    }
+
+    audio {
+      position: absolute;
+      width: 300px;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%);
+      border: none;
+    }
+    
+    #recorderContainer {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      border: none;
+      white-space: nowrap;
+      text-align: center;
+      margin: 100px 0px;
+    }
+
+    .logo {
+      position: absolute;
+      top: 30%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 90px;
+      height: 90px;
+      z-index: 10;
+    }
+
+    a {
+      text-decoration: none;
+      border: 1px solid red;
+      color: red;
+      font-size: 23px;
+    }
+
+    button {
+      color: white;
+      font-size: 16px;
+      background: black;
+      border: 1px solid white;
+      margin: 5px 5px 5px 5px;
+    
+    }
+    
+    .active_button {
+     font-size: 9px;
+     font-weight: bold;
+     background: black;
+     border: 2px solid white;
+
+    }
+
+    b {
+      color: lime;
+      font-size: 24px;
+      
+      }
+  </style>
+</head>
+
+<body>
+
+  <div class="container">
+    <audio id="lecteur" controls autoplay>
+      <source src="https://webradio.tda.dz/Bejaia_64K.mp3" type="audio/mpeg">
+      Votre navigateur ne supporte pas l'élément audio.
+    </audio>
+    <img src="https://www.dz-algerie.info/wp-content/uploads/2022/08/bejaia.png" alt="Logo" class="logo">
+  </div>
+  <div id="recorderContainer">
+    <div id="recordingTime">Temps d'enregistrement : 00:00</div>
+    <button id="startRecording">Démarrer l'enregistrement</button>
+    <button id="stopRecording" disabled>Arrêter l'enregistrement</button><br>
+    <div id="downloadLinkContainer"></div>
+
+  </div>
+  <script src="player.js"></script>
+</body>
+
+</html>
