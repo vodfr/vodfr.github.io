3a3348e18c764256c6f58012fd71b46ab2564991
vodfr <capuccinuroney@gmail.com>
2024-11-07 08:42:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -379,7 +379,7 @@ function updateSpeed() {
 
 function startSpeedTest() {
   startTime = new Date().getTime();
-  timer = setTimeout(updateSpeed, 3000);
+  timer = setTimeout(updateSpeed, 2000);
 }
 function controls() {
   bar.style.opacity = "1";
